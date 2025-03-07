4b95ba8cc2126ab9051315f1df9fdabc66d2b5e9
mitchell <91237766+boring-jpg@users.noreply.github.com>
2025-03-07 01:48:23 -0500

    remove src, add a few tests

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'pyddns',
+    name = 'py_ddns',
     version= '0.1.0',
     author = 'Mitchell Johnson',
+    license= 'MIT',
     author_email = '91237766+mitjohnson@users.noreply.github.com.',
     description = 'Simple programmable DDNS client to automate IP adress changes.',
     long_description = open('README.md').read(),
@@ -12,10 +13,8 @@ setup(
     packages = find_packages(where='src'),
     classifiers = [
         'Programming Language :: Python :: 3',
-        'License :: MIT License',
         'Operating System :: OS Independent',
     ],
-    install_requires = [
-        'cloudflare',
-    ]
+    install_requires = ['cloudflare'],
+    package_dir={'': 'ddns'},
 )
